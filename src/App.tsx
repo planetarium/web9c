0dd7d08e5e46378168226345b58895ae8a1885c4
moreal <dev.moreal@gmail.com>
2023-05-24 19:14:31 +0900

    style: apply prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Layout } from "./layouts/Layout";
 function App() {
   return (
     <Layout>
-      <h1 className="text-3xl font-bold animate-scrollText p-4">Hi There! 안녕하세요! おはよう!</h1>
+      <h1 className="text-3xl font-bold animate-scrollText p-4">
+        Hi There! 안녕하세요! おはよう!
+      </h1>
     </Layout>
   );
 }

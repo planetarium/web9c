0107f41d830214ea42bdf58bff4aea6a51b63bfc
moreal <dev.moreal@gmail.com>
2023-05-31 21:03:23 +0900

    Add margin for buttons

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,7 +2,7 @@ type ButtonProps = React.PropsWithChildren<React.ComponentProps<"button">>;
 
 export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button className="p-2 bg-black text-white" {...props}>
+    <button className="p-2 m-2 bg-black text-white" {...props}>
       {children}
     </button>
   );

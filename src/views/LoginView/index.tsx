1eeadce15d44dff8b9999f9eff7c47a5b4df621e
moreal <dev.moreal@gmail.com>
2023-06-02 17:58:24 +0900

    Use @planetarium/account-web3-secret-storage

--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -1,16 +1,19 @@
 import { Layout } from "../../layouts/Layout";
-import { LOCAL_STORAGE_KEY } from "../../constants";
 import { Navigate, useNavigate } from "react-router";
 import Button from "../../components/ui/Button";
-import { isKeyObject } from "../../web3-account";
-import { decryptKeyObject } from "../../web3-account";
 import useAccountContext from "../../hooks/useAccountContext";
 import { useForm } from "react-hook-form";
 import InputField from "../../components/ui/InputField";
+import useAccountMetadatas, {
+  createWeb3KeyStore,
+} from "../../hooks/useAccountMetadatas";
 
 interface Inputs {
+  keyId: string;
   password: string;
 }
+
+const keyStore = createWeb3KeyStore();
 
 export default function LoginView() {
   const navigate = useNavigate();
@@ -18,30 +21,60 @@ export default function LoginView() {
 
   const { setPrivateKey } = useAccountContext();
 
-  const protectedPrivateKeyItem = localStorage.getItem(LOCAL_STORAGE_KEY);
-  if (protectedPrivateKeyItem == null) {
+  const accountMetadatas = useAccountMetadatas(keyStore);
+
+  if (accountMetadatas == null) {
+    return <p>Checking keys...</p>;
+  }
+
+  if (accountMetadatas.length < 1) {
     return <Navigate to="/welcome" />;
   }
 
-  // eslint-disable-next-line
-  const maybeProtectedPrivateKey = JSON.parse(protectedPrivateKeyItem!);
-  if (!isKeyObject(maybeProtectedPrivateKey)) {
-    return <Navigate to="/welcome" />;
-  }
+  function onSubmit(
+    { keyId, password }: Inputs,
+    e?: React.BaseSyntheticEvent
+  ): Promise<void> {
+    console.log("call onSubmit");
+    e?.preventDefault();
 
-  function onSubmit(inputs: Inputs) {
-    decryptKeyObject(maybeProtectedPrivateKey, inputs.password)
-      .then(({ privateKey }) => {
-        setPrivateKey(privateKey);
-        navigate("/lobby");
-      })
-      .catch(() => alert("Invalid password."));
+    const keyStore = createWeb3KeyStore(() => password);
+    return keyStore.get(keyId).then((result) => {
+      console.log("result", result, password);
+      if (result.result === "success") {
+        return result.account
+          .exportPrivateKey()
+          .then((privateKey) => {
+            console.log("get private key");
+            setPrivateKey(privateKey);
+            navigate("/lobby");
+            return;
+          })
+          .catch(console.error);
+      } else if (result.result === "error") {
+        console.log("error");
+        throw new Error("error " + result.message);
+      } else if (result.result === "keyNotFound") {
+        console.log("keyNotFound");
+        throw new Error("Key not found");
+      }
+
+      throw new Error("Unexpected error");
+    });
   }
 
   return (
     <Layout>
       <h1>Login</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <select {...register("keyId", { required: true })}>
+          {...accountMetadatas.map(({ keyId, address }) => (
+            <option key={keyId} value={keyId}>
+              {address.toString()} ({keyId})
+            </option>
+          ))}
+        </select>
+        <br />
         <InputField
           type="password"
           {...register("password", { required: true })}
--- src/views/WelcomeView/index.tsx
+++ src/views/WelcomeView/index.tsx
@@ -1,21 +1,28 @@
 import { ButtonLink } from "../../components/ui/ButtonLink";
-import { LOCAL_STORAGE_KEY } from "../../constants";
+import useAccountMetadatas, {
+  createWeb3KeyStore,
+} from "../../hooks/useAccountMetadatas";
 import { Layout } from "../../layouts/Layout";
 
+const keyStore = createWeb3KeyStore();
+
 export default function WelcomeView() {
-  const registered = localStorage.getItem(LOCAL_STORAGE_KEY) != null;
+  const accountMetadatas = useAccountMetadatas(keyStore);
+
+  if (accountMetadatas == null) {
+    return <p>Checking accounts...</p>;
+  }
+
+  const registered = accountMetadatas.length > 0;
 
   return (
     <Layout>
       <h1>Hello, web9c</h1>
-      {registered ? (
-        <ButtonLink to="/login">Login</ButtonLink>
-      ) : (
-        <>
-          <ButtonLink to="/register">Generate your own key</ButtonLink>
-          <ButtonLink to="/import">Import your own key</ButtonLink>
-        </>
-      )}
+      <>
+        {registered && <ButtonLink to="/login">Login</ButtonLink>}
+        <ButtonLink to="/register">Generate your own key</ButtonLink>
+        <ButtonLink to="/import">Import your own key</ButtonLink>
+      </>
     </Layout>
   );
 }

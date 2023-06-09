41ba7bd8792e6a6cce6c6fe40021ef54ccf57a36
moreal <dev.moreal@gmail.com>
2023-06-09 11:58:56 +0900

    Separate equipment sections

--- src/views/AvatarView/tabs/InventoryTab/tabs/EquipmentTab.tsx
+++ src/views/AvatarView/tabs/InventoryTab/tabs/EquipmentTab.tsx
@@ -7,15 +7,15 @@ interface EquipmentTabProps {
   onClickItem: (itemId: string) => void;
 }
 
-export default function EquipmentTab({
+const EquipmentSection = ({
   items,
-  selectedItemIds,
   onClickItem,
-}: EquipmentTabProps) {
-  function additionalClassName(itemId: string): string {
-    return selectedItemIds.includes(itemId) ? " bg-blue-300/[0.20]" : "";
-  }
-
+  additionalClassName,
+}: {
+  items: Equipment[];
+  onClickItem: (itemid: string) => void;
+  additionalClassName: (itemId: string) => string;
+}) => {
   return (
     <>
       {items.map((x) => (
@@ -32,5 +32,65 @@ export default function EquipmentTab({
         </div>
       ))}
     </>
+  );
+};
+
+export default function EquipmentTab({
+  items,
+  selectedItemIds,
+  onClickItem,
+}: EquipmentTabProps) {
+  function additionalClassName(itemId: string): string {
+    return selectedItemIds.includes(itemId) ? " bg-blue-300/[0.20]" : "";
+  }
+
+  const sectionCommonProps = {
+    onClickItem,
+    additionalClassName,
+  } as const;
+
+  function compareEquipment(a: Equipment, b: Equipment): number {
+    if (a.grade > b.grade) {
+      return -1;
+    }
+
+    if (a.grade < b.grade) {
+      return 1;
+    }
+
+    if (a.level > b.level) {
+      return -1;
+    }
+
+    if (a.level < b.level) {
+      return 1;
+    }
+
+    return 0;
+  }
+  const ITEM_SUB_TYPES = [
+    "Weapon",
+    "Armor",
+    "Belt",
+    "Necklace",
+    "Ring",
+  ] as const;
+
+  return (
+    <div>
+      {...ITEM_SUB_TYPES.map((type) => (
+        <div key={type}>
+          <h3>{type}</h3>
+          <div>
+            <EquipmentSection
+              items={items
+                .sort(compareEquipment)
+                .filter((x) => x.itemSubType === type)}
+              {...sectionCommonProps}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }

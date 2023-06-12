9074a2869b20dd80ca52d3ba0589f9bf5ecb7752
moreal <dev.moreal@gmail.com>
2023-06-12 16:11:17 +0900

    Notice when failed to login

--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 
 type InputFieldProps = React.ComponentProps<"input">;
+type PasswordFieldProps = {
+  invalid?: boolean;
+  message?: string;
+} & InputFieldProps;
+
+export const PasswordField = React.forwardRef<
+  HTMLInputElement,
+  PasswordFieldProps
+>(({ invalid, message, ...props }, ref) => {
+  const borderColor = invalid ? "border-red-800" : "border-black";
+  return (
+    <div className="inline-block p-2">
+      <input
+        ref={ref}
+        className={`block mt-2 border-solid ${borderColor} border`}
+        {...props}
+      />
+      {message && <span className="absolute">{message}</span>}
+    </div>
+  );
+});
 
 const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
   (props, ref) => {
     return (
       <input
         ref={ref}
-        className="ml-2 border-solid border-black border"
+        className="block mt-2 border-solid border-black border"
         {...props}
       />
     );
--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -3,10 +3,11 @@ import { Navigate, useNavigate } from "react-router";
 import Button from "../../components/ui/Button";
 import useAccountContext from "../../hooks/useAccountContext";
 import { useForm } from "react-hook-form";
-import InputField from "../../components/ui/InputField";
+import { PasswordField } from "../../components/ui/InputField";
 import useAccountMetadatas, {
   createWeb3KeyStore,
 } from "../../hooks/useAccountMetadatas";
+import { useState } from "react";
 
 interface Inputs {
   keyId: string;
@@ -22,6 +23,7 @@ export default function LoginView() {
   const { setPrivateKey, setAddress } = useAccountContext();
 
   const accountMetadatas = useAccountMetadatas(keyStore);
+  const [loginError, setLoginError] = useState<string>();
 
   if (accountMetadatas == null) {
     return <p>Checking keys...</p>;
@@ -51,18 +53,22 @@ export default function LoginView() {
           })
           .then((address) => {
             setAddress(address);
+            setLoginError(undefined);
             navigate("/lobby");
             return;
           })
-          .catch(console.error);
+          .catch((e) => setLoginError(e.message));
       } else if (result.result === "error") {
         console.log("error");
+        setLoginError("Something wrong.");
         throw new Error("error " + result.message);
       } else if (result.result === "keyNotFound") {
         console.log("keyNotFound");
+        setLoginError("Key not found");
         throw new Error("Key not found");
       }
 
+      setLoginError("Something wrong.");
       throw new Error("Unexpected error");
     });
   }
@@ -79,8 +85,10 @@ export default function LoginView() {
           ))}
         </select>
         <br />
-        <InputField
+        <PasswordField
           type="password"
+          invalid={loginError !== undefined}
+          message={loginError}
           {...register("password", { required: true })}
         />
         <Button type="submit">Login</Button>

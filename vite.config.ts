9474c6302421af1889bb67909c35af1991e6fa61
Akamig <akamig@posteo.net>
2023-06-21 00:22:44 +0900

    Add vite-plugin-ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr(), tsconfigPaths(), ssr({ prerender: true })],
   build: {
     // to make tests faster
     minify: false,

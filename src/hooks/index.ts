aa2ad409f0b75db6ab11a6124df2de77efbb6c7f
Akamig <akamig@posteo.net>
2023-07-04 18:05:10 +0900

    Remove Hooks

--- src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { useAccountMetadatas } from "./useAccountMetadatas";
-export { useAvatarStates } from "./useAvatarStates";
-export { useNcgBalance } from "./useGoldBalance";
-export { useInterval } from "./useInterval";
-export { useNextTxNonce } from "./useNextTxNonce";
-export { useTransactionResult } from "./useTransactionResult";
-export { useAvatarInventory } from "./useAvatarInventory";
-export { useRawState } from "./useRawState";
